770681040b5ae0c126fb88f0064fbe433f3e849c
JeroenVinke <jeroenvinke17@gmail.com>
2017-06-29 21:03:47 +0200

    1.6.1

--- jspm_packages/npm/aurelia-kendoui-bridge@1.6.0.js
+++
@@ -1,3 +0,0 @@
-define(["npm:aurelia-kendoui-bridge@1.6.0/index"], function(main) {
-  return main;
-});
---
+++ jspm_packages/npm/aurelia-kendoui-bridge@1.6.1.js
@@ -0,0 +1,3 @@
+define(["npm:aurelia-kendoui-bridge@1.6.1/index"], function(main) {
+  return main;
+});
